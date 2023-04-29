2fe81b7ac7e4f15d55b2db4411a1ab6673f57f62
Mellanie-Marques <103280572+Mellanie-Marques@users.noreply.github.com>
2023-04-29 17:27:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# criando-componente
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-yvjhev)
---
+++ src/layout/layout.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    MatButtonModule,
+    MatMenuModule
+
+  ]
+})
+export class LayoutModule { }
